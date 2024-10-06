82d62ccbee82d631f50009c4dc02efec0c790d60
unknown <mandarmarathe2001@gmail.com>
2024-10-06 16:41:48 +0530

    update

--- apps/api/src/environments/environment.ts
+++ apps/api/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  MONGO_URI:'mongodb+srv://mandarmarathe2001:I4XIMoT6GLdIWlvb@clustersuitespot.sd54x.mongodb.net/?retryWrites=true&w=majority&appName=Clustersuitespot',
+  MONGO_URI:'you mongo url',
   JWT_SECRET:'suiteSpot',
   JWT_EXPIRES_IN:'1d'
 };
